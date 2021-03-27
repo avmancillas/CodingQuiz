522a4aca7ea697af5adda921b56ac809fba8d8b0
Avi Mancillas <avmancillas@gmail.com>
2021-03-26 23:45:43 -0500

    Update ReadMe

---
+++ assets/js/questions.js
@@ -0,0 +1,33 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: "What are the Boolean data type values?",
+    choices: ["strings", "True and False", "symbols", "numbers"],
+    answer: "True and False"
+  },
+  {
+    title: "JQuery is to JavaScript, like Bootstrap is to ____.",
+    choices: ["HTML", "ReadMe files", "CSS", "GitHub"],
+    answer: "CSS"
+  },
+  {
+    title: "What number does the index of an Array start with?",
+    choices: [
+      "2",
+      "1",
+      "0",
+      "all of the above"
+    ],
+    answer: "0"
+  },
+  {
+    title: "Inside what HTML elements do you put JavaScript:",
+    choices: ["<Script>", "<JS>", "<JavaScript>", "<JScript>"],
+    answer: "<Script>"
+  },
+  {
+    title: "What is the format to add comments to JavaScript?",
+    choices: ["//*insert comment here", "//insert comment here//", "//!insert comment here", "//insert comment here"],
+    answer: "//insert comment here"
+  }
+];
